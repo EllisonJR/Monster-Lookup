4a01eadb7217eaa2653cbf73c59887c7123e4a03
Kronack <kronack1@gmail.com>
2019-12-28 15:31:56 -0500

    Add project files.

---
+++ Monster LookUp/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Monster_LookUp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.monsterSizes = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 356);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 25;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(590, 289);
+            this.dataGridView1.StandardTab = true;
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.TabStop = false;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick_PopulateMonsterData);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.TableUpdate);
+            // 
+            // monsterSizes
+            // 
+            this.monsterSizes.FormattingEnabled = true;
+            this.monsterSizes.Items.AddRange(new object[] {
+            "Tiny",
+            "Small",
+            "Medium",
+            "Large",
+            "Huge",
+            "Gargantuan",
+            "Colossal"});
+            this.monsterSizes.Location = new System.Drawing.Point(320, 14);
+            this.monsterSizes.Name = "monsterSizes";
+            this.monsterSizes.Size = new System.Drawing.Size(121, 21);
+            this.monsterSizes.TabIndex = 5;
+            this.monsterSizes.SelectedIndexChanged += new System.EventHandler(this.TableUpdate);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1238, 397);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(654, 633);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1042);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.monsterSizes);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox monsterSizes;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Monster LookUp/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+namespace Monster_LookUp
+{
+    public partial class Form1 : Form
+    {
+        BindingSource bindingSource1 = new BindingSource();
+        SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter();
+
+        string createTempTableString;
+        string insertIntoTempTableString;
+        string mainQueryString;
+        string textBoxString;
+        string dropTableString;
+
+        string additionalString;
+
+        string fullQuery;
+
+        string monsterNameQuery;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            fullQuery = createTempTableString + insertIntoTempTableString + mainQueryString + textBoxString + dropTableString;
+
+            TableUpdate(this, null);
+        }
+        private void TableUpdate(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = bindingSource1;
+
+            
+            GetData(QueryCompiler());
+
+            dataGridView1.Columns[0].Width = 100;
+            dataGridView1.Columns[1].Width = 100;
+            dataGridView1.Columns[2].Width = 100;
+            dataGridView1.Columns[3].Width = 100;
+            dataGridView1.Columns[4].Width = 100;
+            dataGridView1.Columns[5].Width = 100;
+        }
+
+        private string QueryCompiler()
+        {
+            additionalString = "";
+            createTempTableString = "CREATE TEMPORARY TABLE backMonsters(monsterName, challengeRating, armorClass, hitpointAverage, monsterType, size);";
+            insertIntoTempTableString = "INSERT INTO backMonsters SELECT monsterName, challengeRating, armorClass, hitpointAverage, monsterType, size FROM Monsters;";
+            mainQueryString = "SELECT monsterName, challengeRating, armorClass, hitpointAverage, monsterType, size FROM backMonsters WHERE monsterName LIKE ";
+            textBoxString = "'" + textBox1.Text + "%'";
+
+            if (monsterSizes.Text == "Tiny")
+            {
+                additionalString += " AND size = 'Tiny'";
+            }
+            if (monsterSizes.Text == "Small")
+            {
+                additionalString += " AND size = 'Small'";
+            }
+            if (monsterSizes.Text == "Medium")
+            {
+                additionalString += " AND size = 'Medium'";
+            }
+            if (monsterSizes.Text == "Large")
+            {
+                additionalString += " AND size = 'Large'";
+            }
+            if (monsterSizes.Text == "Huge")
+            {
+                additionalString += " AND size = 'Huge'";
+            }
+            if (monsterSizes.Text == "Gargantuan")
+            {
+                additionalString += " AND size = 'Gargantuan'";
+            }
+            if (monsterSizes.Text == "Colossal")
+            {
+                additionalString += " AND size = 'Colosssal'";
+            }
+            additionalString += ";";
+            dropTableString = "DROP TABLE backMonsters;";
+
+             return fullQuery = createTempTableString + insertIntoTempTableString + mainQueryString + textBoxString + additionalString + dropTableString;
+        }
+        private void GetData(string selectCommand)
+        {
+            try
+            {
+                // Specify a connection string.  
+                // Replace <SQL Server> with the SQL Server for your Northwind sample database.
+                // Replace "Integrated Security=True" with user login information if necessary.
+                String connectionString = "Data Source=Monsters.db; Version = 3; New = True; Compress = True;";
+
+                // Create a new data adapter based on the specified query.
+                dataAdapter = new SQLiteDataAdapter(selectCommand, connectionString);
+
+                // Create a command builder to generate SQL update, insert, and
+                // delete commands based on selectCommand. 
+                SQLiteCommandBuilder commandBuilder = new SQLiteCommandBuilder(dataAdapter);
+
+                // Populate a new data table and bind it to the BindingSource.
+                DataTable table = new DataTable
+                {
+                    Locale = CultureInfo.InvariantCulture
+                };
+                dataAdapter.Fill(table);
+                bindingSource1.DataSource = table;
+
+                // Resize the DataGridView columns to fit the newly loaded content.
+                dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+            }
+            catch(SQLiteException)
+            {
+
+            }
+        }
+        private Byte[] GrabMonsterImage(string selectCommand)
+        {
+            Byte[] image = null;
+            SQLiteDataReader monsterInfo;
+            String connectionString = "Data Source=Monsters.db; Version = 3; New = True; Compress = True;";
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                using (SQLiteCommand command = new SQLiteCommand(selectCommand, connection))
+                {
+                    using (monsterInfo = command.ExecuteReader())
+                    {
+                        while (monsterInfo.Read())
+                        {
+                            if (monsterInfo["image"] != null && !Convert.IsDBNull(monsterInfo["image"]))
+                            {
+                                image = (Byte[])monsterInfo["image"];
+                            }
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return image;
+        }
+        private string GrabMonsterDescription(string selectCommand)
+        {
+            string description = "";
+            SQLiteDataReader monsterInfo;
+            String connectionString = "Data Source=Monsters.db; Version = 3; New = True; Compress = True;";
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                using (SQLiteCommand command = new SQLiteCommand(selectCommand, connection))
+                {
+                    using (monsterInfo = command.ExecuteReader())
+                    {
+                        while (monsterInfo.Read())
+                        {
+                            if (monsterInfo["image"] != null && !Convert.IsDBNull(monsterInfo["image"]))
+                            {
+                                description = (string)monsterInfo["image"];
+                            }
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return description;
+        }
+        private void dataGridView1_CellClick_PopulateMonsterData(object sender, DataGridViewCellEventArgs e)
+        {
+            monsterNameQuery = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[0].Value.ToString();
+
+            Byte[] data = GrabMonsterImage("SELECT image FROM monsters WHERE monsterName='" + monsterNameQuery + "'");
+            MemoryStream mem = new MemoryStream();
+            if (data != null)
+            {
+                mem = new MemoryStream(data);
+                pictureBox1.Image = Image.FromStream(mem);
+            }
+            else
+            {
+                pictureBox1.Image = null;
+            }
+            
+
+        }
+    }
+}
